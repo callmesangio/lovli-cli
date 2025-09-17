d90f862c2d418629fd8ad2821e873d5eb763bf8c
Fabio Sangiovanni <f@sangiovanni.dev>
2025-09-17 12:14:30 +0200

    Implement POST.

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 const endpoint string = "https://lovli.test/redirections"
+
+type Redirection struct {
+	ShortUrl string `json:"short_url"`
+}
 
 func main() {
 	parseCli()
@@ -17,7 +26,13 @@ func main() {
 	if err != nil {
 		fail(err)
 	}
-	fmt.Println(*url)
+
+	redirection, err := post(url)
+	if err != nil {
+		fail(err)
+	}
+
+	fmt.Println(redirection.ShortUrl)
 }
 
 func parseCli() {
@@ -43,4 +58,46 @@ func url() (*string, error) {
 func fail(err error) {
 	fmt.Println(err)
 	os.Exit(1)
+}
+
+func post(url *string) (*Redirection, error) {
+	client := http.Client{Timeout: 10 * time.Second}
+	req := newRequest(url)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusOK {
+		return jsonDecode(res.Body)
+	}
+	return nil, newPostError(res.StatusCode)
+}
+
+func newRequest(url *string) *http.Request {
+	body := fmt.Appendf([]byte{}, `{"location": "%s"}`, *url)
+	req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(body))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	return req
+}
+
+func jsonDecode(body io.ReadCloser) (*Redirection, error) {
+	redirection := &Redirection{}
+	err := json.NewDecoder(body).Decode(redirection)
+	if err != nil {
+		return nil, err
+	}
+	return redirection, nil
+}
+
+func newPostError(statusCode int) error {
+	switch statusCode {
+	case http.StatusBadRequest:
+		return errors.New("Invalid URL")
+	case http.StatusTooManyRequests:
+		return errors.New("Try again later")
+	default:
+		return fmt.Errorf("Unexpected error (%d)", statusCode)
+	}
 }
