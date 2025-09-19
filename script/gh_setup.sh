8471dfc98c74c55f38e7f0f88411b4885fc6df3f
Fabio Sangiovanni <f@sangiovanni.dev>
2025-09-19 11:59:08 +0200

    Update GH setup script.

--- script/gh_setup.sh
+++ script/gh_setup.sh
@@ -4,3 +4,5 @@ gh repo edit --enable-projects=false
 gh repo edit --enable-merge-commit=false
 gh repo edit --enable-rebase-merge=false
 gh repo edit --delete-branch-on-merge=true
+gh repo edit --enable-wiki=false
+gh repo edit --enable-discussions=false
